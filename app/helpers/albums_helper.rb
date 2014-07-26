f46fa14fdcaaf18221a67fae1a8284d212b9c8e1
Isra <isra@miscorreos.org>
2014-07-26 17:52:40 +0000

    Fin de los ejercicios del viernes

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,13 @@
 module AlbumsHelper
+  def simbol_rating_tag(rep, char)
+    res = ""
+    rep.to_i.times { res+= char}
+    content_tag(:span, res)
+  end
+
+  def img_rating_tag(rep, img="/img/start.png")
+    res = ""
+    rep.to_i.times { res+= image_tag(img, size: "16x10")}
+    res.to_s.html_safe
+  end
 end
